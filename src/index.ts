cf09a6b0a847ab3f1fe32e86b000700229de2114
gyarasu <dev.yoshitsugu@gmail.com>
2019-12-08 18:18:20 +0900

    feat: aixos-cors-plugin

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import _Vue from 'vue';
+import axios from 'axios';
+import * as qs from 'qs';
+
+axios.interceptors.request.use(config => {
+  if (
+    config.method &&
+    (config.method.toLocaleLowerCase() === 'post' ||
+      config.method.toLocaleLowerCase() === 'put')
+  ) {
+    config.headers['content-type'] =
+      'application/x-www-form-urlencoded;charset=utf-8';
+    config.data = qs.stringify(config.data);
+  }
+
+  return config;
+});
+
+// TODO: add options argument
+const install = () => (Vue: typeof _Vue): void => {
+  Vue.prototype.$axios = axios;
+};
+
+const AxiosCors = {
+  install
+};
+
+export default AxiosCors;
